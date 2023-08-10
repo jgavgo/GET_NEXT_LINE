6b250bf116f78bdaac10e726577a0aa9c0e49c3c
jgavgo <120314544+jgavgo@users.noreply.github.com>
2023-08-10 20:55:57 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: jgavilan <jgavilan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 21:09:30 by jgavilan          #+#    #+#             */
-/*   Updated: 2023/08/08 20:17:22 by jgavilan         ###   ########.fr       */
+/*   Updated: 2023/08/10 20:54:19 by jgavilan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*ft_free(char **buffer)
 {
@@ -23,29 +24,53 @@ char	*ft_readline(char *data, int fd, int flag)
 {
 	char	*buff;
 
-	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	flag = read(fd, buff, BUFFER_SIZE);
+	while (flag > 0 && strchr)
+		flag = read(fd, buff, BUFFER_SIZE);
 	if (flag < 0)
 		return (NULL);
 	if (flag == 0)
-		return ()
+		return (data);
+	
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*data;
 	char		*line;
+	int			flag;
 
-	if (fd < 0)
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	if (!data)
-	{
-		data = (char *)malloc(1);
-		if (!data)
-			return (NULL);
-		data[0] = '\0';
-	}
-	data = ft_readline(data, fd, 1);
+	flag = read(fd, data, BUFFER_SIZE)
+	if ((data && !ft_strchar(data, '\n')) || !data)
+		data = ft_readline(data, fd, 1);
+
+	// hola que tal estas '\n' jaja	
+	line = substr(data, 0, posicion de '\n');
+	//funcion que devuelva hasta el '\n'
+
+	data = substr(data, pos '\n', longitud total);
+	//funcion que elimine de la estatica la linea que acabas de enviar
+	//, es decir, substr desde '\n' hasta el final
+	return (line);
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+int	main(void)
+{
+	char	*arr;
+	char	res[BUFFER_SIZE];
+	int	fd = open("hola.txt", O_RDONLY);
+
+	arr = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	arr = get_next_line(fd);
+	printf("%s", arr);
+	arr = get_next_line(fd);
+	printf("%s", arr);
+	close(fd);
+	return (0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: jgavilan <jgavilan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 22:18:33 by jgavilan          #+#    #+#             */
-/*   Updated: 2023/08/08 18:30:20 by jgavilan         ###   ########.fr       */
+/*   Updated: 2023/08/10 18:15:21 by jgavilan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,57 @@ char	*ft_strchar(const char *data, char c)
 	return (NULL);
 }
 
-char *ft_substr()
-size_t ft_strlen()
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		lens1;
+	int		lens2;
+	int		i;
+
+	if (s1 && s2)
+	{
+		lens1 = ft_strlen(s1);
+		lens2 = ft_strlen(s2);
+		str = malloc((lens1 + lens2 + 1) * sizeof(char));
+		if (!str)
+			return (NULL);
+		i = -1;
+		while (s1[++i] && s1[i] != '\0')
+			str[i] = s1[i];
+		i = -1;
+		while (s2[++i] && s2[i] != '\0')
+		{
+			str[lens1] = s2[i];
+			lens1++;
+		}
+		str[lens1] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
+
+int bytes = 1;
+char *buffer;
+char *line;
+
+buffer = ft_calloc(sizeof(char) * BUFFER_SIZE +1);
+(!buffer)
+	return (-1);
+
+while(bytes > 0 || ft_strchar(buffer, '\n'))
+{
+	free(buffer);
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	line = ft_strjoin(line, buffer);
+}
+
